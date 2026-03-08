87c49f6cdc4c3664e74d2ec8e4429a84f7d92ffe
Syed Kaleem <kaleemsyed.sk@hotmail.com>
2026-03-07 19:26:29 -0500

    headless

--- fetch_gold_rate.py
+++ fetch_gold_rate.py
@@ -15,11 +15,13 @@ DATA_FILE = "gold_rates.json"
 
 
 options = Options()
+
 options.add_argument("--headless=new")
-options.add_argument("--incognito")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920,1080")
+options.add_argument("--incognito")
 
 driver = webdriver.Chrome(options=options)
 
