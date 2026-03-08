c369457521df71b88a74997d6a355e4f667fc75a
Syed Kaleem <kaleemsyed.sk@hotmail.com>
2026-03-07 19:21:46 -0500

    headless

--- fetch_gold_rate.py
+++ fetch_gold_rate.py
@@ -13,9 +13,13 @@ url = "https://www.goodreturns.in/gold-rates/"
 
 DATA_FILE = "gold_rates.json"
 
+
 options = Options()
+options.add_argument("--headless=new")
 options.add_argument("--incognito")
-options.add_argument("--start-maximized")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--window-size=1920,1080")
 
 driver = webdriver.Chrome(options=options)
 
